a70bfe260a0035c18a61ce085d48374eff23cd18
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-07-04 18:33:58 -0300

    resolução do requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Loading from '../components/Loading';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.state = {
+      searchLoading: false,
+      artistInput: '',
       artistSearch: '',
+      responseAPI: '',
     };
   }
 
@@ -21,9 +26,26 @@ class Search extends Component {
     });
   }
 
-  render() {
-    const { loading } = this.props;
+  handleClick = () => {
     const { artistSearch } = this.state;
+    const param = artistSearch;
+    this.setState({
+      artistInput: param,
+      artistSearch: '',
+      searchLoading: true,
+    }, async () => {
+      const response = await searchAlbumsAPI(param);
+
+      this.setState({
+        searchLoading: false,
+        responseAPI: response,
+      });
+    });
+  }
+
+  render() {
+    const { loading, handleSubmit } = this.props;
+    const { artistSearch, artistInput, searchLoading, responseAPI } = this.state;
     const SEARCH_VALIDATION = 2;
 
     return (
@@ -33,25 +55,52 @@ class Search extends Component {
             <Header
               loading={ loading }
             />
-            <form>
-              <label htmlFor="artistSearch">
-                <input
-                  type="text"
-                  data-testid="search-artist-input"
-                  name="artistSearch"
-                  value={ artistSearch }
-                  onChange={ this.handleChange }
-                />
-              </label>
-              <button
-                type="submit"
-                data-testid="search-artist-button"
-                disabled={ artistSearch.length < SEARCH_VALIDATION }
-              >
-                Pesquisar
+            {searchLoading ? <Loading /> : (
+              <>
+                <form onSubmit={ handleSubmit }>
+                  <label htmlFor="artistSearch">
+                    <input
+                      type="text"
+                      data-testid="search-artist-input"
+                      name="artistSearch"
+                      value={ artistSearch }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <button
+                    type="submit"
+                    data-testid="search-artist-button"
+                    disabled={ artistSearch.length < SEARCH_VALIDATION }
+                    onClick={ this.handleClick }
+                  >
+                    Pesquisar
 
-              </button>
-            </form>
+                  </button>
+                </form>
+                {responseAPI && (
+                  <>
+                    <p>
+                      Resultado de álbuns de:
+                      {` ${artistInput}`}
+                    </p>
+                    {responseAPI.length === 0 ? <h2>Nenhum álbum foi encontrado</h2>
+                      : (responseAPI.map((element) => (
+                        <Link
+                          to={ `/album/${element.collectionId}` }
+                          key={ element.collectionId }
+                          data-testid={ `link-to-album-${element.collectionId}` }
+                        >
+                          <div>
+                            <img src={ element.artworkUrl100 } alt="album cover" />
+                            <h3>{element.artistName}</h3>
+                            <p>{element.collectionName}</p>
+                          </div>
+                        </Link>
+                      )))}
+                  </>
+                )}
+              </>
+            )}
           </>
         )}
       </div>
@@ -61,6 +110,7 @@ class Search extends Component {
 
 Search.propTypes = {
   loading: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default Search;
